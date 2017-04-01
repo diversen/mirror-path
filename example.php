02469cb2d1847ad4af336d7ccc45931f9dcfcf07
Dennis Iversen <dennis.iversen@gmail.com>
2017-04-01 12:42:50 +0200

    Update example.php

--- example.php
+++ example.php
@@ -5,5 +5,5 @@ use diversen\mirrorPath;
 $m = new mirrorPath();
 // $m->deleteBefore = true; // Delete before mirroring. Default setting
 // $m->allowTypes = array ('md', 'php'); // Allow md and php files
-// $m->disallowTypes = array('md'); // disallow md files. Only php files will be mirrored
+// $m->disallowTypes = array('md'); // disallow md files. Now only php files will be mirrored
 $m->mirror('./vendor', './vendor2'); // mirror
